e51fdb457bd5d512d7f4c9f025ffc027598a62e8
ramos-zip <enzoramos40953@gmail.com>
2026-08-25 12:25:53 -0300

    working...

--- EKmusic/screens/MusicPlayer.js
+++ EKmusic/screens/MusicPlayer.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { FlatList, Image, StyleSheet, Text, useWindowDimensions, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-import colors from '../theme/colors'
+import { songs } from '../model/data';
+import colors from '../theme/colors';
 
 export default function MusicPlayer() {
-  return (
+    const { width } = useWindowDimensions();
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const currentSong = songs[selectedIndex];
+    const artworkSize = Math.min(width - 40, 380);
+
+    function handleMomentumEnd(event) {
+        const offset = event.nativeEvent.contentOffset.x;
+        const index = Math.round(offset / width);
+        setSelectedIndex(index);
+    }
+
+    function renderArtwork({ item }) {
+        return (
+            <View style ={[styles.artworkPage, { width }]}>
+                <Image 
+                    source={item.artwork}
+                    style={[styles.artwork,
+                        { width: artworkSize, height: artworkSize},
+                    ]}
+                />
+            </View>
+        )
+    }
+
+    return (
     <SafeAreaView style={styles.container}>
         <View style={styles.content}>
             <Text style={styles.eyebrown}>Tocando agora..</Text>
